6fed1fc9b970a2382c0b273f7c026a57b04b737a
Samuel Geissinger <samuelmgeissinger@gmail.com>
2022-03-22 14:45:49 -0600

    Addition implementation working

--- calc.js
+++ calc.js
@@ -10,10 +10,30 @@ const reader = readline.createInterface({
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
 	
-	mathSymbol = tokens[0];
+	let mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
+
+	let result = 0;
+	if (mathSymbol === '+') {
+		result = num1 + num2;
+	} else if (mathSymbol === '-') {
+		result = Math.abs(num1 - num2);
+	} else if (mathSymbol === '*') {
+		result = num1 * num2;
+	} else if (mathSymbol === '/') {
+		result = num1 / num2;
+	} else if (mathSymbol === 'sqrt') {
+		result = Math.sqrt(num1, num2);
+	} else {
+		console.log("Invalid symbol")
+	}
+	console.log(result);
 
 	// This line closes the connection to the command line interface.
 	reader.close()
